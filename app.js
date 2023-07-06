4081c7b6b11376b1c0a85f4db1a438c2cfc7a102
Olga Boris <olya_boris97@mail.ru>
2023-07-06 17:24:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const now = new Date();
+const output = document.getElementById('output');
+const fullBtn = document.getElementById('full');
+const dateBtn = document.getElementById('date');
+const timeBtn = document.getElementById('time');
+
+output.textContent = new Date();
